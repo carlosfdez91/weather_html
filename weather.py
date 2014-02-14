843e2bb38767ea3f8fc4ccc36b293412645964c0
carlosfdez91 <carlosfernandez16@gmail.com>
2014-02-14 12:20:08 +0100

    Añadido varias líneas

--- weather.py
+++ weather.py
@@ -1,8 +1,21 @@
 #coding: utf-8
 import requests
 import json
+import webbrowser
 from jinja2 import Template
-plantilla = open("plantilla.html","w")
+plantilla = open("plantilla.html","r")
+
+html = ''
+
+for linea in html:
+	html += linea
+
+miplantilla = Template(html)
+miplantilla.render(capitales='capital',temp_min='temp_min',temp_max='temp_max',viento='redon',orientacion='direccion_viento')
+
+print html
+fresultado = open('resultado.html','w')
+fresultado.write('miplantilla')
 
 def orientacion(direccion):
 	"""Función que calcula la dirección de la que procede el viento"""
@@ -26,15 +39,16 @@ def orientacion(direccion):
 
 capitales = ["Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla"]
 
-respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % capitales[7]})
+for elemento in capitales:
+	respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % elemento})
+	dicc = json.loads(respuesta.text)
+	temp_min = dicc["main"]["temp_min"] - 273
+	temp_max = dicc["main"]["temp_max"] - 273
+	viento = round(dicc["wind"]["speed"] * 1.61,2)
+	redon = round(viento, 2)
+	direccion = dicc["wind"]["deg"]
 
-dicc = json.loads(respuesta.text)
+webbrowser.open("resultado.html")
 
-tempmin = dicc["main"]["temp_min"] - 273
-tempmax = dicc["main"]["temp_max"] - 273
-viento = dicc["wind"]["speed"] * 1.61
-redon = round(viento, 2)
-direccion = dicc["wind"]["deg"]
-
-print "La temparatura mínima de %s es de %s ºC, la máxima es de %s ºC\ny el viento es de %s km/h Dirección %s " % (capitales[7],tempmin,tempmax,redon,orientacion(direccion))
+#print "La temparatura mínima de %s es de %s ºC, la máxima es de %s ºC\ny el viento es de %s km/h Dirección %s " % (capitales[7],temp_min,temp_max,redon,orientacion(direccion))
 
