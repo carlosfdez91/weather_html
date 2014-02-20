8c84f2a9281451b4694cc42bdd7a00929bbe6731
carlosfdez91 <carlosfernandez16@gmail.com>
2014-02-20 10:21:47 +0100

    Version casi definitiva, aun con fallos graves

--- weather.py
+++ weather.py
@@ -3,16 +3,19 @@ import requests
 import json
 import webbrowser
 from jinja2 import Template
-plantilla = open("plantilla.html","r")
-ncapitales = 0
+plan = open("plantilla.html","r")
+capitales = ['Almeria','Cadiz','Cordoba','Granada','Huelva','Jaen','Malaga','Sevilla']
+num_capitales = 0
 
 html = ''
-
-for linea in plantilla:
+for linea in plan:
 	html = html + linea
-
 plantilla = Template(html)
 
+lista_temp_min = []
+lista_temp_max = []
+lista_redon = []
+lista_direccion = []
 
 def orientacion(direccion):
 	"""Función que calcula la dirección de la que procede el viento"""
@@ -34,20 +37,30 @@ def orientacion(direccion):
 		elif direccion >= 292.5 and direccion < 337.5:
 			return "NO"
 
-capitales = ["Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla"]
+while num_capitales <= 7:
+	fichero = requests.get('http://api.openweathermap.org/data/2.5/weather/',params={'q':'%s,spain' %capitales[num_capitales]})
+	dicc = json.loads(fichero.text)
 
-while ncapitales <= 7:
-	fichero = requests.get('http://api.openweathermap.org/data/2.5/weather/',params={'q':'%s,spain' %capitales[ncapitales]})
-	dicc = json.loads(respuesta.text)
-	temp_min = dicc["main"]["temp_min"] - 273
-	temp_max = dicc["main"]["temp_max"] - 273
+
+	temp_min = round(dicc["main"]["temp_min"] - 273)
+	temp_max = round(dicc["main"]["temp_max"] - 273)
 	viento = round(dicc["wind"]["speed"] * 1.61,2)
 	redon = round(viento, 2)
 	direccion = dicc["wind"]["deg"]
 
-plantilla_sal = plantilla.render(ncapitales='capitales',temp_min='temp_min',temp_max='temp_max',viento='redon',orientacion='direccion_viento')
+	lista_temp_min.append(temp_min)
+	lista_temp_max.append(temp_max)
+	lista_redon.append(redon)
+	lista_direccion.append(orientacion)
 
-webbrowser.open("resultado.html")
+	num_capitales = num_capitales + 1
+
+plantilla_salida = plantilla.render(capitales_num=capitales,temp_min=lista_temp_min,temp_max=lista_temp_max,speed=lista_redon,direccion_viento=lista_direccion)
+
+resultado=open('plantilla_salida.html','w')
+resultado.write(plantilla_salida)
+resultado.close()
+webbrowser.open("plantilla_salida.html")
 
 #print "La temparatura mínima de %s es de %s ºC, la máxima es de %s ºC\ny el viento es de %s km/h Dirección %s " % (capitales[7],temp_min,temp_max,redon,orientacion(direccion))
 
